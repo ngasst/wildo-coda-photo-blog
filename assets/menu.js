11f5eb014574d7dd1f86ced842a4d4fc2ac8eb62
Jenny <jennyelfort3@gmail.com>
2019-05-11 17:05:32 +0200

    menu.js completed

---
+++ assets/menu.js
@@ -0,0 +1,20 @@
+const items = [
+    {
+        text : "Home", 
+        href : "index.html"
+    },
+    {
+        text : "About us", 
+        href : "about-us.html"
+    }, 
+    {
+        text : "Category",
+        href : "category.html"
+    },
+    {
+        text : "Contact", 
+        href : "contact.html"
+    }
+]
+console.log(items); 
+createMenu("#nav-menu", items);
